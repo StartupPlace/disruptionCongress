831e152a41d31970e5a3bc2715f3fd4baae9369d
Elvis <Elvis@MacBook-Pro-de-Elvis.local>
2014-11-24 03:10:18 -0500

    modificado el puerto

--- index.js
+++ index.js
@@ -47,8 +47,10 @@ app.get('/', function (req, res) {
     res.render('index.html')
 });
 
+var port = process.env.PORT || 3000;
 
-var server = app.listen(3000, function () {
+
+var server = app.listen(port, function () {
 
     var host = server.address().address
     var port = server.address().port
